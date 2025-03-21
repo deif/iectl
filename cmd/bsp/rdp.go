f4eebff11ba53399c0411a7a1befe2d7de7ae7b5
Kristian Mide <kmi@deif.com>
2025-03-21 12:43:56 +0100

    bsp: we have rdp status, enable and disable

---
+++ cmd/bsp/rdp.go
@@ -0,0 +1,125 @@
+package bsp
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rdpCmd = &cobra.Command{
+	Use:       "rdp",
+	Short:     "Get rdp status or enable/disable",
+	Args:      cobra.OnlyValidArgs,
+	ValidArgs: []cobra.Completion{"enable", "disable", "status"},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		// no arguments? - receive rdp status
+		if len(args) == 0 || args[0] == "status" {
+			return getrdpStatus(cmd, args)
+		}
+
+		client := cmd.Context().Value(aClientKey).(*http.Client)
+		host, _ := cmd.Flags().GetString("hostname")
+		u := url.URL{
+			Scheme: "https",
+			Host:   host,
+			Path:   "/bsp/service/rdp",
+		}
+
+		var enable bool
+		if args[0] == "enable" {
+			enable = true
+		}
+
+		reqStruct := struct {
+			Running bool `json:"running"`
+		}{
+			Running: enable,
+		}
+
+		body, err := json.Marshal(reqStruct)
+		if err != nil {
+			return fmt.Errorf("unable to marshal request body: %w", err)
+		}
+
+		req, err := http.NewRequest("PUT", u.String(), bytes.NewReader(body))
+		if err != nil {
+			return fmt.Errorf("unable to create http put request: %w", err)
+		}
+
+		resp, err := client.Do(req)
+		if err != nil {
+			return fmt.Errorf("unable to http post: %w", err)
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			return fmt.Errorf("unexpected http status code: %d", resp.StatusCode)
+		}
+		asJson, _ := cmd.Flags().GetBool("json")
+		if !asJson {
+			fmt.Println("Device answers: 200 OK")
+		}
+
+		// nothing for --json. users must deal with the exit code of iecli
+		return nil
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(rdpCmd)
+}
+
+func getrdpStatus(cmd *cobra.Command, args []string) error {
+	client := cmd.Context().Value(aClientKey).(*http.Client)
+	host, _ := cmd.Flags().GetString("hostname")
+	u := url.URL{
+		Scheme: "https",
+		Host:   host,
+		Path:   "/bsp/service/rdp",
+	}
+
+	resp, err := client.Get(u.String())
+	if err != nil {
+		return fmt.Errorf("unable to http get: %w", err)
+	}
+	defer resp.Body.Close()
+
+	switch resp.StatusCode {
+	case http.StatusOK:
+	default:
+		return fmt.Errorf("unexpected statuscode: %d", resp.StatusCode)
+	}
+
+	asJson, _ := cmd.Flags().GetBool("json")
+	if asJson {
+		_, err = io.Copy(os.Stdout, resp.Body)
+		if err != nil {
+			return fmt.Errorf("unable to copy to stdout: %w", err)
+		}
+		return nil
+	}
+
+	dec := json.NewDecoder(resp.Body)
+	response := &struct {
+		Running bool `json:"running"`
+	}{}
+
+	err = dec.Decode(response)
+	if err != nil {
+		return fmt.Errorf("unable to unmarshal response: %w", err)
+	}
+
+	if response.Running {
+		fmt.Println("RDP Service: enabled")
+	} else {
+		fmt.Println("RDP Service: disabled")
+	}
+
+	return nil
+}

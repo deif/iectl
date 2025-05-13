e1a68199663eeec89070b5ec9e07a1760f4e7bbc
Kristian Mide <kmi@deif.com>
2025-05-13 13:22:48 +0200

    bsp factory-reset: multitarget support

--- cmd/bsp/factoryreset.go
+++ cmd/bsp/factoryreset.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 	"net/url"
 
-	"github.com/deif/iectl/auth"
+	"github.com/deif/iectl/target"
 	"github.com/spf13/cobra"
 )
 
@@ -13,25 +13,26 @@ var factoryResetCmd = &cobra.Command{
 	Use:   "factory-reset",
 	Short: "Factory reset device",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		client := auth.FromContext(cmd.Context())
-		host, _ := cmd.Flags().GetString("hostname")
-		u := url.URL{
-			Scheme: "https",
-			Host:   host,
-			Path:   "/bsp/system/reset",
-		}
+		targets := target.FromContext(cmd.Context())
+		for _, target := range targets {
+			u := url.URL{
+				Scheme: "https",
+				Host:   target.Hostname,
+				Path:   "/bsp/system/reset",
+			}
 
-		resp, err := client.Post(u.String(), "application/binary", nil)
-		if err != nil {
-			return fmt.Errorf("unable to http post: %w", err)
-		}
-		defer resp.Body.Close()
+			resp, err := target.Client.Post(u.String(), "application/binary", nil)
+			if err != nil {
+				return fmt.Errorf("unable to http post: %w", err)
+			}
+			defer resp.Body.Close()
 
-		switch resp.StatusCode {
-		case http.StatusOK:
-		case http.StatusAccepted:
-		default:
-			return fmt.Errorf("unexpected statuscode: %d", resp.StatusCode)
+			switch resp.StatusCode {
+			case http.StatusOK:
+			case http.StatusAccepted:
+			default:
+				return fmt.Errorf("unexpected statuscode: %d", resp.StatusCode)
+			}
 		}
 
 		return nil

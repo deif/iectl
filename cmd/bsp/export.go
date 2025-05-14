db0b654b65d7f2813e95235facb190def7403a82
Kristian Mide <kristian@mide.dk>
2025-05-14 14:42:49 +0200

    bsp export: added example

--- cmd/bsp/export.go
+++ cmd/bsp/export.go
@@ -10,7 +10,26 @@ import (
 
 var exportCmd = &cobra.Command{
 	Use:   "export",
-	Short: "export variables and targets",
+	Short: "Export targets and credentials",
+	Long: `Export the variables and targets in a format that can be programmatically evaluated.
+
+The exported values include IECTL_BSP_TARGET, IECTL_BSP_USERNAME, and IECTL_BSP_PASSWORD.
+
+If these values are set in the environment where iectl is executed,
+iectl can automatically determine the appropriate targets.
+This eliminates the need to specify the --target option with each
+command or to rely on mDNS for target discovery.
+
+This approach provides a more reliable and deterministic way to specify
+exactly which targets should be used, avoiding the unpredictability
+that can sometimes occur with mDNS.
+
+Examples:
+
+  Save all currently responding targets into the environment:
+
+    $ eval $(iectl bsp export --target-all)
+`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		targets := target.FromContext(cmd.Context())
 		var t = make([]string, 0)

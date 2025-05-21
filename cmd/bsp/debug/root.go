d31314ae8101107e7004b6dbebfb6bb8e395d910
Nikolaj Jensen Ulrik <nju@deif.com>
2025-05-21 14:15:57 +0200

    rewrite to new targets API, better response handling

--- cmd/bsp/debug/root.go
+++ cmd/bsp/debug/root.go
@@ -4,12 +4,13 @@ import (
 	"bytes"
 	"fmt"
 	"io"
-	"unicode/utf8"
+	"os"
+	"strings"
 
 	"net/http"
 	"net/url"
 
-	"github.com/deif/iectl/auth"
+	"github.com/deif/iectl/target"
 	"github.com/spf13/cobra"
 )
 
@@ -31,9 +32,13 @@ var getCmd = &cobra.Command{
 	Short: "Compose GET request",
 	Args:  cobra.OnlyValidArgs,
 	RunE: func(cmd *cobra.Command, args []string) error {
+		targets := target.FromContext(cmd.Context())
+		if len(targets) > 1 {
+			return fmt.Errorf("refusing to debug request with more than 1 target")
+		}
+		target := targets[0]
+
 		quiet := false
-		unsafePrint := false
-		client := auth.FromContext(cmd.Context())
 
 		if len(args) == 0 {
 			return fmt.Errorf("missing required argument path")
@@ -41,22 +46,21 @@ var getCmd = &cobra.Command{
 
 		reqBody := getBody(args[1:])
 
-		host, _ := cmd.Flags().GetString("hostname")
 		interactive, _ := cmd.Flags().GetBool("interactive")
 
 		path := args[0]
 		u := url.URL{
 			Scheme: "https",
-			Host:   host,
+			Host:   target.Hostname,
 			Path:   "/bsp/" + path,
 		}
 
-		req, err := http.NewRequest("GET", u.String(), bytes.NewReader([]byte (reqBody)))
+		req, err := http.NewRequest("GET", u.String(), bytes.NewReader([]byte(reqBody)))
 		if err != nil {
 			return fmt.Errorf("unable to create http request: %w", err)
 		}
 
-		resp, err := client.Do(req)
+		resp, err := target.Client.Do(req)
 		if err != nil {
 			return fmt.Errorf("unable to http get: %w", err)
 		}
@@ -66,19 +70,27 @@ var getCmd = &cobra.Command{
 			fmt.Printf("Request GET to %s completed.\n", path)
 		}
 
-		respBody, err := io.ReadAll(resp.Body)
-		if err != nil {
-			return fmt.Errorf("error reading http response: %w", err)
+		showBody := true
+		if interactive {
+			if resp.Header.Get("Content-Type") == "application/zip" {
+				fmt.Println("Response indicates a binary Content-Type, do you still want to print it? [y/N]")
+				var ans string
+				fmt.Scanln(&ans)
+				if !strings.HasPrefix(strings.ToLower(ans), "y") {
+					showBody = false
+				}
+			}
 		}
 
 		fmt.Println("status-code: ", resp.StatusCode)
-		if !unsafePrint && !utf8.Valid(respBody) {
-			fmt.Printf("body: <binary data, length %d>", len(respBody))
-			if interactive {
-				// TODO ask to save to file?
+		fmt.Print("body: ")
+		if showBody {
+			_, err := io.Copy(os.Stdout, resp.Body)
+			if err != nil {
+				return fmt.Errorf("error reading http response: %w", err)
 			}
 		} else {
-			fmt.Printf("body: %s\n", respBody)
+			fmt.Printf("<binary data, Content-Length %s>", resp.Header.Get("Content-Type"))
 		}
 
 		return nil

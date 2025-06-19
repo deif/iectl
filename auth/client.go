dd39f880e5ace0b3be586b837c560b1b1dc5ce63
Kristian Mide <kmi@deif.com>
2025-06-19 13:40:49 +0200

    auth.Client: we have ability to refresh our jwt token, thereby allowing for install times exceeding its expire time

--- auth/client.go
+++ auth/client.go
@@ -10,6 +10,7 @@ import (
 	"net"
 	"net/http"
 	"net/url"
+	"sync"
 	"time"
 )
 
@@ -84,24 +85,84 @@ func Client(host, user, pass string, insecure bool) (*http.Client, error) {
 		return nil, fmt.Errorf("unexpected http status code: %d", resp.StatusCode)
 	}
 
+	var refreshToken string
+	cookies := resp.Cookies()
+	for _, v := range cookies {
+		if v.Name == "refresh_token" {
+			refreshToken = v.Value
+		}
+	}
+
 	t := &authTransport{
 		RoundTripper: c.Transport,
 		token:        resp.Header.Get("Authorization"),
+		refreshToken: refreshToken,
 	}
+
+	// if the refreshToken is non empty, initialize a keepalive routine
+	if refreshToken != "" {
+		go t.keepalive(u)
+	}
+
 	c.Transport = t
 
 	return c, nil
 }
 
 type authTransport struct {
+	sync.RWMutex
 	http.RoundTripper
-	token string
+	token           string
+	refreshToken    string
+	refreshTokenErr error
 }
 
 func (a *authTransport) RoundTrip(req *http.Request) (*http.Response, error) {
 	// Clone request to avoid modifying the original one
 	clonedReq := req.Clone(req.Context())
+
+	a.RLock()
 	clonedReq.Header.Set("Authorization", a.token)
+	a.RUnlock()
 
 	return a.RoundTripper.RoundTrip(clonedReq)
+}
+
+func (a *authTransport) keepalive(url url.URL) {
+	url.Path = "/auth/refresh"
+	for {
+		// TODO: use actual expire value from JWT
+		// for now, we know its usually 10 minuttes, so we refresh at every 9 minuttes
+		time.Sleep(time.Minute * 9)
+		req, err := http.NewRequest("GET", url.String(), nil)
+		if err != nil {
+			a.refreshTokenErr = fmt.Errorf("refresh token: could not create http request: %w", err)
+			break
+		}
+
+		c := &http.Cookie{
+			Name:  "refresh_token",
+			Value: a.refreshToken,
+		}
+		req.AddCookie(c)
+
+		httpClient := &http.Client{Transport: a}
+		resp, err := httpClient.Do(req)
+		if err != nil {
+			a.refreshTokenErr = fmt.Errorf("refresh token: request failed: %w", err)
+			break
+		}
+
+		// the body have nothing of interest
+		resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			a.refreshTokenErr = fmt.Errorf("refresh token: request returned unexpected status code: %d", resp.StatusCode)
+			break
+		}
+
+		a.Lock()
+		a.token = resp.Header.Get("Authorization")
+		a.Unlock()
+	}
 }
